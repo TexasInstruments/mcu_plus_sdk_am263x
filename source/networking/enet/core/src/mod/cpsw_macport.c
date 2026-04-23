cb9ea461ce8714d253f3d740614a05b899035bb4
epswbld5 <epswbld5@ti.com>
2026-04-23 19:43:51 +0000

    SDK Nightly build 2026-04-23
    List of changes from previous build
    
    ## mcu_plus_sdk
    Previous: 89559c219137 -> Current: 42fdbb0de935
    
    42fdbb0de93 am64x: projectspec: Update genProjectSpec.js
    
    ## enet_lld
    Previous: 7d9a6fbda781 -> Current: a046a4d17047
    
    a046a4d17 J722S: fix release profile lib build
    
    Signed-off-by: epswbld5 <epswbld5@ti.com>

--- source/networking/enet/core/src/mod/cpsw_macport.c
+++ source/networking/enet/core/src/mod/cpsw_macport.c
@@ -1302,7 +1302,6 @@ static int32_t CpswMacPort_configSgmii(CSL_CpsgmiiRegs *sgmiiRegs,
                                        const EnetMacPort_LinkCfg *linkCfg)
 {
     CSL_SGMII_ADVABILITY sgmiiAdvAbility;
-    CSL_SGMII_STATUS sgmiiStatus;
     uint32_t portNum = ENET_MACPORT_NORM(macPort);
     uint32_t portId = ENET_MACPORT_ID(macPort);
     int32_t status = ENET_SOK;
@@ -1382,12 +1381,8 @@ static int32_t CpswMacPort_configSgmii(CSL_CpsgmiiRegs *sgmiiRegs,
         CSL_SGMII_enableMasterMode(sgmiiRegs, portNum);
         CSL_SGMII_disableAutoNegotiation(sgmiiRegs, portNum);
 
-        /* Wait for SGMII link */
-        do
-        {
-            CSL_SGMII_getStatus(sgmiiRegs, portNum, &sgmiiStatus);
-        }
-        while (sgmiiStatus.bIsLinkUp != 1U);
+        /*Removed the wait for SGMII linkup
+        TODO: need to add the logic in Cpsw_periodicTick API*/
 
         status = ENET_SOK;
     }
@@ -1512,6 +1507,8 @@ static int32_t CpswMacPort_checkSgmiiStatus(CSL_CpsgmiiRegs *sgmiiRegs,
                 /* Get link partner speed/duplexity */
                 CSL_SGMII_getLinkPartnerAdvAbility(sgmiiRegs, portNum, &sgmiiAdvAbility);
             }
+            ENETTRACE_VAR(*CpswMacPort_gSgmiiSpeedNames);
+            ENETTRACE_VAR(*CpswMacPort_gSgmiiDuplexNames);
 
             ENETTRACE_INFO("MAC %u: SGMII link parter config port: link %s: %s %s\n",
                            portId, sgmiiAdvAbility.bLinkUp ? "up" : "down",
