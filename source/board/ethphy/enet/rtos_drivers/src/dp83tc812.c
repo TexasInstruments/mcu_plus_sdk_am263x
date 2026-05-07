7f9395200ec7203c9beca0d6797cdf736b12b6d8
epswbld5 <epswbld5@ti.com>
2026-05-07 09:04:32 +0000

    SDK REL.MCUSDK.26.00.00.05 Release Package
    List of changes from previous build
    
    ## mcu_plus_sdk
    Previous: ac8b21c6253c -> Current: 3eb5041c2b9e
    
    3eb5041c2b9 am26xx: sdl: update release notes for 26.00.00
    240ba2bd9f8 am263x/am263px: sdl/ecc: fix the build error
    
    Signed-off-by: epswbld5 <epswbld5@ti.com>

--- source/board/ethphy/enet/rtos_drivers/src/dp83tc812.c
+++ source/board/ethphy/enet/rtos_drivers/src/dp83tc812.c
@@ -55,7 +55,7 @@
 #define DP83TC812_REV_CS2                     (1U)
 #define DP83TC813_MODEL                       (0x21U)
 #define DP83TC814_MODEL                       (0x26U)
-#define DP83TC815_MODEL                       (0x2EU)
+#define DP83TC815_MODEL                       (0x20U)
 
 /* ========================================================================== */
 /*                         Structure Declarations                             */
@@ -212,7 +212,6 @@ bool Dp83tc812_isMacModeSupported(EthPhyDrv_Handle hPhy,
     switch (mii)
     {
         case PHY_MAC_MII_MII:
-            supported = true;
             break;
         case PHY_MAC_MII_GMII:
             break;
@@ -220,9 +219,6 @@ bool Dp83tc812_isMacModeSupported(EthPhyDrv_Handle hPhy,
             supported = true;
             break;
         case PHY_MAC_MII_SGMII:
-            supported = true;
-            break;
-        case PHY_MAC_MII_RMII:
             supported = true;
             break;
         default:
@@ -785,10 +781,10 @@ int32_t Dp83tc812_getSpeedDuplex (EthPhyDrv_Handle hPhy, Phy_Link_SpeedDuplex* p
     Phy_RegAccessCb_t* pRegAccessApi = PhyPriv_getRegAccessApi(hPhy);
 
     /* Restart is complete when RESET bit has self-cleared */
-    status = pRegAccessApi->EnetPhy_readReg(pRegAccessApi->pArgs, PHY_BMSR, &val);
+    status = pRegAccessApi->EnetPhy_readReg(pRegAccessApi->pArgs, DP83TC812_PHYSTS, &val);
     if (status == PHY_SOK)
     {
-        if (val & PHY_BIT(2))
+        if (val & DP83TC812_PHYSTS_LINK)
         {
             speed = 100;
             *pConfig = PHY_LINK_FD100;
